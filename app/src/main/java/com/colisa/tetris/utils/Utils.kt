08776060810c693d29d3cee1838295510f3ad864
mtali <emmanuelmtali@yahoo.com>
2023-02-12 22:28:11 +0300

    Add utils and sound effects :fire:

--- app/src/main/java/com/colisa/tetris/utils/Utils.kt
+++ app/src/main/java/com/colisa/tetris/utils/Utils.kt
@@ -1,10 +1,89 @@
 package com.colisa.tetris.utils
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.media.AudioManager
+import android.media.SoundPool
+import android.view.View
+import android.view.WindowManager
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.colisa.tetris.R
+
+
+fun Offset(x: Int, y: Int) = Offset(x.toFloat(), y.toFloat())
+
+
+enum class Direction {
+    Left, Up, Right, Down
+}
+
+fun Direction.toOffset() = when (this) {
+    Direction.Left -> -1 to 0
+    Direction.Up -> 0 to -1
+    Direction.Right -> 1 to 0
+    Direction.Down -> 0 to 1
+}
+
+val NextMatrix = 4 to 2
+const val ScoreEverySpirit = 12
+
+@Suppress("DEPRECATION")
+object StatusBarUtil {
+    fun transparentStatusBar(activity: Activity) {
+        with(activity) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            val option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            val vis = window.decorView.systemUiVisibility
+            window.decorView.systemUiVisibility = option or vis
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+}
+
+@SuppressLint("StaticFieldLeak")
+object SoundUtil {
+
+    private var context: Context? = null
+    private val sp: SoundPool by lazy {
+        SoundPool.Builder().setMaxStreams(4).setMaxStreams(AudioManager.STREAM_MUSIC).build()
+    }
+    private val map = mutableMapOf<SoundType, Int>()
+
+    fun init(context: Context) {
+        this.context = context
+        Sounds.forEach {
+            map[it] = sp.load(context, it.res, 1)
+        }
+    }
+
+    fun release() {
+        context = null
+        sp.release()
+    }
+
+    fun play(isMute: Boolean, soundType: SoundType) {
+        sp.play(requireNotNull(map[soundType]), 1f, 1f, 0, 0, 1f)
+    }
+}
+
+sealed class SoundType(val res: Int) {
+    object Move : SoundType(R.raw.move)
+    object Rotate : SoundType(R.raw.rotate)
+    object Start : SoundType(R.raw.start)
+    object Drop : SoundType(R.raw.drop)
+    object Clean : SoundType(R.raw.clean)
+}
+
+val Sounds =
+    listOf(SoundType.Move, SoundType.Rotate, SoundType.Start, SoundType.Drop, SoundType.Clean)
+
 
 val LedFontFamily = FontFamily(
     Font(R.font.unidream_led, FontWeight.Light),
